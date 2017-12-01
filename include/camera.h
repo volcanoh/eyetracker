2064cc75ad230552bc1a3e0f123a79fc4376e969
volcanoh <huang.volcano@gmail.com>
2017-12-01 10:26:32 +0800

    camera devilsved from cv::VideoCapture

--- include/camera.h
+++ include/camera.h
@@ -2,26 +2,15 @@
 #include <opencv2/opencv.hpp>
 #include <object.h>
 
-class Camera : public Object {
+class Camera : public Object, public cv::VideoCapture {
 public:
-  Camera(int idx);
-  Camera(Camera&);
-  void SetId(int idx);
-  int GetId();
-
-  bool Open();
-  void Close();
-  bool IsOpened();
+  Camera();
+  Camera(const std::string& filename);
+  Camera(int device);
 
   void GetCameraMatrix(cv::Mat& camMat);
   void SetCameraMatrix(const cv::Mat& camMat);
 
-  void GetImage(cv::Mat& image);
-  Camera& operator >> (cv::Mat& image);
-
  private:
-  int index_;
-
-  cv::VideoCapture videoCapture_;
   cv::Mat cameraMatrix_;
 };
