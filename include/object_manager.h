1b95ee07ac78bfc6fb7bb271c6f3aef0df3bc0bb
volcanoh <huang.volcano@gmail.com>
2017-12-04 11:19:34 +0800

    move MatRt to object_manager

--- include/object_manager.h
+++ include/object_manager.h
@@ -2,43 +2,44 @@
 
 #include <object.h>
 
-struct MatRt {
-  cv::Matx31d rvec;
-  cv::Matx31d tvec;
-  MatRt(const cv::Matx31d rv, const cv::Matx31d tv) :
-    rvec(rv),
-    tvec(tv) {
-    }
-  MatRt(){}
-  MatRt& operator=(const MatRt& m) {
-    rvec = m.rvec;
-    tvec = m.tvec;
-    return *this;
-  }
-};
-
 class ObjectManager {
- public:
-  using IdPair = std::pair<int, int>;
+  public:
+    struct MatRt {
+      cv::Matx31d rvec;
+      cv::Matx31d tvec;
+      MatRt(const cv::Matx31d rv, const cv::Matx31d tv) :
+        rvec(rv),
+        tvec(tv) {
+        }
+      MatRt(){}
+      MatRt& operator=(const MatRt& m) {
+        rvec = m.rvec;
+        tvec = m.tvec;
+        return *this;
+      }
+    };
 
-  static ObjectManager& GetInstance() {
-    return instance_;
-  }
 
-  int RegisterObject(Object*);
+    using IdPair = std::pair<int, int>;
 
-  void SetTransformation(int id1, int id2, const cv::Matx31d& rvec, const cv::Matx31d& tvec);
-  bool GetTransformation(int id1, int id2, cv::Matx31d& rvec, cv::Matx31d& tvec);
+    static ObjectManager& GetInstance() {
+      return instance_;
+    }
 
- private:
-  static ObjectManager instance_;
-  ObjectManager();
-  ~ObjectManager();
+    int RegisterObject(Object*);
 
-  std::map<IdPair, MatRt> transformation_maps_; // access tranformation mat by id-id
-  std::map<int, Object*> objects_; // access object by id
+    void SetTransformation(int id1, int id2, const cv::Matx31d& rvec, const cv::Matx31d& tvec);
+    bool GetTransformation(int id1, int id2, cv::Matx31d& rvec, cv::Matx31d& tvec);
 
-  int CreateObjectId();
+  private:
+    static ObjectManager instance_;
+    ObjectManager();
+    ~ObjectManager();
+
+    std::map<IdPair, MatRt> transformation_maps_; // access tranformation mat by id-id
+    std::map<int, Object*> objects_; // access object by id
+
+    int CreateObjectId();
 };
 
 
