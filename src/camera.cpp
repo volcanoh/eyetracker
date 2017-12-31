798e4a72d191059e80d2a3985bfe6e6bbb6006e6
maowei17 <470419503@qq.com>
2017-12-31 11:21:13 +0800

    fix bug in order to make it work.

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,16 +1,16 @@
-//#include "camera.h"
-//
-//void Camera::GetCameraMatrix( cv::Mat &camMat ) {
-//  camMat = cameraMatrix_;
-//}
-//void Camera::SetCameraMatrix( const cv::Mat &camMat ) {
-//  cameraMatrix_ = camMat;
-//}
-//
-//Camera::Camera(){
-//}
-//
-//Camera::Camera(int device) : cv::VideoCapture(device) {
-//}
-//Camera::Camera(const std::string& filename) : cv::VideoCapture(filename) {
-//}
+#include "camera.h"
+
+void Camera::GetCameraMatrix( cv::Mat &camMat ) {
+  camMat = cameraMatrix_;
+}
+void Camera::SetCameraMatrix( const cv::Mat &camMat ) {
+  cameraMatrix_ = camMat;
+}
+
+Camera::Camera(){
+}
+
+Camera::Camera(int device) : cv::VideoCapture(device) {
+}
+Camera::Camera(const std::string& filename) : cv::VideoCapture(filename) {
+}
--- src/object_manager.cpp
+++ src/object_manager.cpp
@@ -1,39 +1,40 @@
-//#include "object_manager.h"
-//
-//
-//ObjectManager::ObjectManager() {
-//}
-//
-//ObjectManager::~ObjectManager() {
-//}
-//
-//ObjectManager ObjectManager::instance_;
-//
-//int ObjectManager::RegisterObject(Object* object) {
-//  if (!object) return -1;
-//  int id = CreateObjectId();
-//  object->SetId(id);
-//  objects_[id] = object;
-//  return id;
-//}
-//
-//int ObjectManager::CreateObjectId() {
-//  static int id = 0;
-//  return id++;
-//}
-//
-//void ObjectManager::SetTransformation(int id1, int id2, const cv::Matx31d& rvec, const cv::Matx31d& tvec) {
-//  MatRt mat_rt(rvec, tvec);
-//  IdPair id_pair = std::make_pair(id1, id2);
-//  transformation_maps_[id_pair] = mat_rt;
-//}
-//
-//bool ObjectManager::GetTransformation(int id1, int id2, cv::Matx31d& rvec, cv::Matx31d& tvec) {
-//  IdPair id_pair = std::make_pair(id1, id2);
-//
-//  auto it = transformation_maps_.find(id_pair);
-//  if (it != transformation_maps_.end()) {
-//    rvec = it->second.rvec;
-//    tvec = it->second.tvec;
-//  }
-//}
+#include "object_manager.h"
+
+
+ObjectManager::ObjectManager() {
+}
+
+ObjectManager::~ObjectManager() {
+}
+
+ObjectManager ObjectManager::instance_;
+
+int ObjectManager::RegisterObject(Object* object) {
+  if (!object) return -1;
+  int id = CreateObjectId();
+  object->SetId(id);
+  objects_[id] = object;
+  return id;
+}
+
+int ObjectManager::CreateObjectId() {
+  static int id = 0;
+  return id++;
+}
+
+void ObjectManager::SetTransformation(int id1, int id2, const cv::Matx31d& rvec, const cv::Matx31d& tvec) {
+  MatRt mat_rt(rvec, tvec);
+  IdPair id_pair = std::make_pair(id1, id2);
+  transformation_maps_[id_pair] = mat_rt;
+}
+
+bool ObjectManager::GetTransformation(int id1, int id2, cv::Matx31d& rvec, cv::Matx31d& tvec) {
+  IdPair id_pair = std::make_pair(id1, id2);
+
+  auto it = transformation_maps_.find(id_pair);
+  if (it != transformation_maps_.end()) {
+    rvec = it->second.rvec;
+    tvec = it->second.tvec;
+  }
+  return true;
+}
